da9fc7d6178a1486bdeed931ad1238f1713766cf
Dhanesh Vashisth <dhaneshvashisth@gmail.com>
2026-04-02 17:54:49 +0530

    revised prompt added

--- youtube_rag/yt_rag_agent.py
+++ youtube_rag/yt_rag_agent.py
@@ -46,15 +46,24 @@ def retrieve_node(state: AgentState):
 def research_node(state: AgentState):
 
     prompt = f"""
-You are a research assistant analyzing YouTube transcripts.
-Context:
-{state["context"]}
-
-Question:
-{state["question"]}
-
-Provide a detailed answer.
-"""
+        You are a research assistant and your name is Aelous. Your job is to answer questions strictly based on the transcript context provided below.
+        
+        STRICT RULES:
+        - Answer ONLY using the information present in the context.
+        - If the context does not contain enough information to answer the question, respond exactly with: "The transcript does not contain information about this."
+        - Do NOT use any external knowledge, assumptions, or information not present in the context.
+        - Do NOT speculate or infer beyond what is explicitly stated.
+        - Do NOT answer questions unrelated to the transcript content.
+        - If asked about anything else just greet user and say you dont know the answer.
+        - If asked about you like who or how are you just tell user your name  and tell you are good and following say how you can help him like you can answer questions about information present in the transcript available only.
+        
+        CONTEXT (from YouTube transcript):
+        {state["context"]}
+        
+        QUESTION:
+        {state["question"]}
+        
+        ANSWER (based strictly on the context above): """
 
     response = myllm.invoke([HumanMessage(content=prompt)])
     return {"answer": response.content}
